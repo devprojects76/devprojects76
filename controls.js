b6e2eab9e76c3fef201a6556d2e81627065afea9
devprojects76 <projectsspecial76@gmail.com>
2025-03-02 22:25:01 -0500

    Update controls.js

--- controls.js
+++ controls.js
@@ -25,7 +25,7 @@
             var table = document.getElementById('reading-list');
             data.forEach(function(object) {
                 var tr = document.createElement('tr');
-                tr.innerHTML = <td>' + object.NAME + '</td>' +
+                tr.innerHTML = '<td>' + object.NAME + '</td>' +
                 '<td>' + object.AUTHOR + '</td>' +
                 '<td>' + object.TYPE + '</td>' +
                 '<td>' + object.SUBJECTAREA + '</td>' +  
