c3ed4aa58ac2794ad5204df15ae7d49323060ad3
devprojects76 <projectsspecial76@gmail.com>
2025-03-02 22:38:24 -0500

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,35 +1,24 @@
-//append json to html function from: https://stackoverflow.com/questions/51275730/populate-html-table-with-json-data
-//first add an event listener for page load
-        document.addEventListener( "DOMContentLoaded", get_json_data, false ); // get_json_data is the function name that will fire on page load
-
-        //this function is in the event listener and will execute on page load
-        function get_json_data(){
-            var json_url = 'readinglist.json';
-
-            xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() { 
-                if (this.readyState == 4 && this.status == 200) {
-
-                    var data = JSON.parse(this.responseText); // convert the response to a json object
-                    append_json(data);// pass the json object to the append_json function
-                }
-            }
-            //set the request destination and type
-            xmlhttp.open("POST", json_url, true);
-            //set required headers for the request
-            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            // send the request
-            xmlhttp.send(); 
-        }
-        function append_json(data){
-            var table = document.getElementById('reading-list');
-            data.forEach(function(object) {
-                var tr = document.createElement('tr');
-                tr.innerHTML = '<td>' + object.NAME + '</td>' +
-                '<td>' + object.AUTHOR + '</td>' +
-                '<td>' + object.TYPE + '</td>' +
-                '<td>' + object.SUBJECTAREA + '</td>' +  
-                '<td>' + object.DATE + '</td>';
-                table.appendChild(tr);
-            });
-        }
+function artFunction() {
+  // Declare starting variables
+  var input = document.getElementById("artInput");
+  var filter = input.value.toUpperCase();
+  var table = document.getElementById("artTable");
+  var trs = table.tBodies[0].getElementsByTagName("tr");
+  // Loop through rows
+  for (var i = 0; i < trs.length; i++) {
+    // Define the cells
+    var tds = trs[i].getElementsByTagName("td");
+    // hide the row
+    trs[i].style.display = "none";
+    // loop through row cells
+    for (var i2 = 0; i2 < tds.length; i2++) {
+      // if there's a match
+      if (tds[i2].innerHTML.toUpperCase().indexOf(filter) > -1) {
+        // show the row
+        trs[i].style.display = "";
+        // skip to the next row
+        continue;
+      }
+    }
+  }
+}
